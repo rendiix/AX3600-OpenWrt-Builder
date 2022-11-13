63d0ec21ef26e7eb4ca7645c2a7293ca101afc01
Rendiix <vanzdobz@gmail.com>
2022-11-13 09:03:41 +0700

    test new partition layout

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@ sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=16553
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/gSpotx2f/luci-app-temp-status.git package/luci-app-temp-status
+
+git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
+svn export --force https://github.com/shidahuilang/openwrt-package/branches/master/openwrt_oscam package/openwrt-packages/openwrt_oscam
+shopt -s extglob
+rm -R -f package/openwrt-packages/!("adguardhome"|"luci-app-adguardhome"|"luci-app-cpufreq"|"luci-app-argon-config"|"luci-app-openvpn-server"|"openwrt_oscam"|"luci-app-oscam"|"qBittorrent"|"luci-app-qbittorrent"|"rblibtorrent"|"qBittorrent-Enhanced-Edition"|"qtbase"|"qttools"|"luci-app-rclone"|"luci-app-zerotier"|"luci-app-wizard"|"vsftpd-alt"|"luci-app-vsftpd"|"luci-theme-argon"|"luci-theme-edge"|"luci-app-easymesh")
 
 mkdir -p files/etc/uci-defaults
 cat > files/etc/uci-defaults/xxx_config << EOF
