9c8b29950572b5f548ca5edfeec872e13f2b095f
rendiix <vanzdobz@gmail.com>
2022-12-30 18:46:34 +0700

    update

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=16553
 
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 #enable wifi
 sed -i 's/.disabled=1/.disabled=0/;s/.ssid=OpenWrt/.ssid=OpenWrt_AX3600/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
