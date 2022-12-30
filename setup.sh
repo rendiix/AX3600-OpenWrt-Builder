4de2639780947b03f2f8c74778f6e3fe37f4f07f
rendiix <vanzdobz@gmail.com>
2022-12-30 15:47:13 +0700

    update

--- setup.sh
+++ setup.sh
@@ -6,21 +6,8 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
-#git clone https://github.com/gSpotx2f/luci-app-temp-status.git package/luci-app-temp-status
-
-#git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
-
-#svn export --force https://github.com/shidahuilang/openwrt-package/branches/master/openwrt_oscam package/openwrt-packages/openwrt_oscam
-
-shopt -s extglob
-rm -R -f package/openwrt-packages/!("luci-app-cpufreq"|"luci-app-argon-config"|"luci-app-openvpn-server"|"luci-app-zerotier"|"luci-theme-argon"|"luci-app-easymesh")
-
-sed -i '$a  \
-CONFIG_CPU_FREQ_GOV_POWERSAVE=y \
-CONFIG_CPU_FREQ_GOV_USERSPACE=y \
-CONFIG_CPU_FREQ_GOV_ONDEMAND=y \
-CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y \
-' target/linux/ipq807x/config-5.15
+#enable wifi
+sed -i 's/.disabled=1/.disabled=0/;s/.ssid=OpenWrt/.ssid=OpenWrt_AX3600/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 mkdir -p files/etc/uci-defaults
 cat > files/etc/uci-defaults/xxx_config << EOF
